5b4fef7ef14afdbdce4f66787bef4b9d44ff8c1f
Cristian Perez Jensen <cristianpjensen@gmail.com>
2024-07-12 13:39:46 +0200

    more efficient calls to google maps api, more scraped coordinates

--- scrape_google_maps_data.py
+++ scrape_google_maps_data.py
@@ -13,47 +13,43 @@ load_dotenv()
 
 def scrape_google_maps_image_and_segmentation(lat: float, lon: float, dir: str):
     satellite_url = get_satellite_url(lat, lon)
-    satellite_img = fetch_image(satellite_url)
+    satellite_imgs = fetch_image(satellite_url)
+    for i, img in enumerate(satellite_imgs):
+        torchvision.io.write_png(img, os.path.join(dir, "images", f"satimage_{lat:.3f},{lon:.3f}_{i}.png"))
 
     segmentation_url = get_segmentation_url(lat, lon)
-    segmentation_img = fetch_image(segmentation_url)
-
-    # Make sure both images can be saved
-    if satellite_img is None or segmentation_img is None:
-        return
-
-    torchvision.io.write_png(satellite_img, os.path.join(dir, "images", f"satimage_{lat:.3f},{lon:.3f}.png"))
-
-    # Round to 0 or 1 and save
-    segmentation_img = (segmentation_img.float() / 255).round()
-    segmentation_img = (segmentation_img * 255).byte()
-    torchvision.io.write_png(segmentation_img, os.path.join(dir, "groundtruth", f"satimage_{lat:.3f},{lon:.3f}.png"))
+    segmentation_imgs = fetch_image(segmentation_url)
+    for i, img in enumerate(segmentation_imgs):
+        # Round to 0 or 1 and save as 8-bit PNG
+        img = (img.float() / 255).round()
+        img = (img * 255).byte()
+        torchvision.io.write_png(img, os.path.join(dir, "groundtruth", f"satimage_{lat:.3f},{lon:.3f}_{i}.png"))
 
 
 def get_segmentation_url(lat: float, lon: float) -> str:
     key = os.environ["GOOGLE_API_KEY"]
-    return f'https://maps.googleapis.com/maps/api/staticmap?key={key}&center={lat:.3f},{lon:.3f}&zoom=18&size=400x422&style=feature:all|color:0x000000&style=feature:road|element:geometry.fill|color:0xFFFFFF&style=feature:road|element:geometry.stroke|color:0xFFFFFF|weight:0.5&style=feature:all|element:labels|visibility:off'
+    return f'https://maps.googleapis.com/maps/api/staticmap?key={key}&center={lat:.3f},{lon:.3f}&zoom=17&size=400x422&scale=2&style=feature:all|color:0x000000&style=feature:road|element:geometry.fill|color:0xFFFFFF&style=feature:road|element:geometry.stroke|color:0xFFFFFF|weight:0.5&style=feature:all|element:labels|visibility:off'
 
 
 def get_satellite_url(lat: float, lon: float) -> str:
     key = os.environ["GOOGLE_API_KEY"]
-    return f'https://maps.googleapis.com/maps/api/staticmap?key={key}&center={lat:.3f},{lon:.3f}&zoom=18&size=400x422&maptype=satellite'
+    return f'https://maps.googleapis.com/maps/api/staticmap?key={key}&center={lat:.3f},{lon:.3f}&zoom=17&size=400x422&scale=2&maptype=satellite'
 
 
-def fetch_image(url: str) -> torch.Tensor | None:
-    try:
-        img_data = requests.get(url).content
-        with tempfile.NamedTemporaryFile() as tmp_file:
-            tmp_file.write(img_data)
-            img = torchvision.io.read_image(tmp_file.name, torchvision.io.ImageReadMode.RGB)
+def fetch_image(url: str) -> list[torch.Tensor]:
+    img_data = requests.get(url).content
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        tmp_file.write(img_data)
+        img = torchvision.io.read_image(tmp_file.name, torchvision.io.ImageReadMode.RGB)
 
-        # Remove Google branding
-        img = img[:, :-22, :]
-        return TF.resize(img, [400, 400], interpolation=TF.InterpolationMode.NEAREST)
+    # Remove Google branding
+    img = img[:, :-44, :]
+    img = TF.resize(img, [800, 800], interpolation=TF.InterpolationMode.NEAREST)
 
-    except RuntimeError:
-        print("Failed to fetch image from URL:", url)
-        return None
+    return [
+        img[:, 0:400, 0:400], img[:, 400:800, 0:400],
+        img[:, 0:400, 400:800], img[:, 400:800, 400:800],
+    ]
 
 
 if __name__ == "__main__":
